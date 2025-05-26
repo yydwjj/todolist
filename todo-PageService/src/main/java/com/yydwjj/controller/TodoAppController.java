5b421ef7770e6eb9a161d79fe328e1053673df0a
郭伟进 <2827348531@qq.com>
2025-05-26 22:15:47 +0800

    为网关适配进行修改

--- todo-PageService/src/main/java/com/yydwjj/controller/TodoAppController.java
+++ todo-PageService/src/main/java/com/yydwjj/controller/TodoAppController.java
@@ -3,8 +3,10 @@ package com.yydwjj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class TodoAppController {
 
 	@GetMapping("/")
