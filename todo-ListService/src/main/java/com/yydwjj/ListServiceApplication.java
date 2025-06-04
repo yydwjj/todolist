33df5c1ad46312d6cb637139115d6672a5322946
郭伟进 <2827348531@qq.com>
2025-06-04 15:53:27 +0800

    调整了一处格式

--- todo-ListService/src/main/java/com/yydwjj/ListServiceApplication.java
+++ todo-ListService/src/main/java/com/yydwjj/ListServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class            ListServiceApplication {
+public class ListServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ListServiceApplication.class, args);
     }
